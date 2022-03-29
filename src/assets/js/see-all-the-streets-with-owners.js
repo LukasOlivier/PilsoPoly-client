1d459634ccf97faff9ee4a29c872574d21d97c51
Student <sibren.eeckhout@student.howest.be>
2022-03-29 13:54:36 +0200

    made a function to fetch the streets from the api server

--- src/assets/js/see-all-the-streets-with-owners.js
+++ src/assets/js/see-all-the-streets-with-owners.js
@@ -4,8 +4,20 @@ document.addEventListener('DOMContentLoaded',init);
 
 function init()
 {
-    /*console.log(testConnection());*/
+   const streets = fetchFromServer('/tiles','GET').then(tiles => runStreets(tiles)).catch(errorHandler)
+}
+
+
+
+function runStreets(tiles)
+{
+    tiles.forEach(street => render_streets(street));
+}
+
+function render_streets(street)
+{
+    console.log(street.name);
     const $template = document.querySelector('template').content.firstElementChild.cloneNode(true);
-    $template.querySelector('h2').innerText = "streetname";
+    $template.querySelector('h2').innerText = street.name;
     document.querySelector('.templatediv').insertAdjacentHTML("beforeend", $template.outerHTML);
 }
