14fbc3af4aacc350fa1e4dc77831d1a7832c8118
xdNiBoR <51537026+xdNiBoR@users.noreply.github.com>
2022-03-29 13:17:54 +0200

    finished js error handling on joining game.

--- src/assets/js/functionality/check-name.js
+++ src/assets/js/functionality/check-name.js
@@ -4,17 +4,12 @@ function joinGame(){
     const $joinInterface = document.querySelector("#join-interface");
     const id = $joinInterface.querySelector("#ID").value;
     const name = {
-        playerName: $joinInterface.querySelector("#name").value
+        playerName: $joinInterface.querySelector("#name").value.toLowerCase()
     };
     // these checks dont see errors from the server
     // we can change this when we make our own api server.
-
-
-    if (checkExistingGames(id, name)) {
+    if (!checkExistingGames(id, name)) {
        return;
-    }
-    if (checkName(name)) {
-        return;
     }
     console.log("Fetching from server");
     fetchFromServer(`/games/${id}/players`,'POST', name)
@@ -22,38 +17,29 @@ function joinGame(){
         // this token is your security token.
        .catch(errorHandler);
 }
-function checkName(name){
-    try {
-        if (name.playerName === "") {
-            throw "Your player name cant be empty!";
-        }
-    }
-    catch (err) {
-        errorHandling(err);
-    }
-}
 
 function checkExistingGames(id, name){
     document.querySelector(".errormessages p").innerText = "";
     fetchFromServer(`/games?prefix=${id}`)
         .then(response => {
             if (response.length === 0) {
-                throw "there is no game with this code";
+                throw "There is no game with this code";
             }
             else if (response.length > 1) {
-                throw "Is your code correct";
+                throw "Please fill in your code correctly";
             }
             else if (response[0].started === true) {
-                throw "Game already started!";
+                throw "Game already started";
             }
             response[0].players.forEach(player => {
-                if (player.name === name.playerName) {
-                    throw "The name is already in use.";
+                if (player.name.toLowerCase() === name.playerName.toLowerCase()) {
+                    throw "The name is already in use";
                 }
             });
+            if (name.playerName === "") {
+                throw "Your player name cant be empty";
+            }
         })
         .catch(errorHandler);
-}
-function errorHandling(errormessage) {
-    document.querySelector(".errormessages p").innerText = errormessage;
+        return false;
 }
