76dff74dcf1dae165e818ec76931b2a3a74b439b
xdNiBoR <51537026+xdNiBoR@users.noreply.github.com>
2022-03-29 22:12:03 +0200

    Fixes in lobby.js file

--- src/assets/js/functionality/lobby.js
+++ src/assets/js/functionality/lobby.js
@@ -3,8 +3,7 @@
 function loadGameDataForLobby(id, name){
     fetchFromServer(`/games?prefix=${id}`)
         .then(response => {
-            console.log(response[0]);
-            const id = response[0].id;
+            const gameID = response[0].id;
             console.log(id);
             const numberOfPlayers = response[0].numberOfPlayers;
             console.log(numberOfPlayers);
