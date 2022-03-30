7f4df7bd78e8afce6be951004820989de04a1099
KoffieLurker <93264203+KoffieLurker@users.noreply.github.com>
2022-03-30 13:24:51 +0200

    Git pull from main

--- src/assets/js/render/start-rendering.js
+++ src/assets/js/render/start-rendering.js
@@ -1,13 +1,38 @@
 "use strict";
 
+let _$startInterface = "";
+let _$createInterface = "";
+let _$joinInterface = "";
+let _$lobbyInterface = "";
+let _$iconInterface = "";
+let _$rulesInterface = "";
+
+
 
 function initStartScreen() {
+    _$startInterface = document.querySelector("#start-interface");
+    _$createInterface = document.querySelector("#create-interface");
+    _$joinInterface = document.querySelector("#join-interface");
+    _$lobbyInterface = document.querySelector("#lobby-interface");
+    _$iconInterface = document.querySelector("#icon-interface");
+    _$rulesInterface = document.querySelector("#rules-interface");
+
+
 
     document.querySelector("#join").addEventListener("click", renderJoin);
     document.querySelector("#create").addEventListener("click", renderCreate);
     document.querySelector("#rules").addEventListener("click", renderRules);
+
+
     document.querySelectorAll('.icon-picker').forEach(item => {
+
+        item.addEventListener('click', function (e) {
+            document.querySelector("#icon-interface").classList.add("hidden");
+            renderIconPicker(e.currentTarget);
+        });
+
         item.addEventListener('click', renderIconPicker);
+
     });
     document.querySelectorAll('.back-button').forEach(item => {
         item.addEventListener('click', backButton);
@@ -15,41 +40,78 @@ function initStartScreen() {
 }
 
 function renderJoin() {
-    document.querySelector("#start-interface").classList.add("hidden");
-    document.querySelector("#create-interface").classList.add("hidden");
-    document.querySelector("#join-interface").classList.remove("hidden");
+    document.querySelector(".errormessages p").innerHTML = "";
+    _$startInterface.classList.add("hidden");
+    _$createInterface.classList.add("hidden");
+    _$joinInterface.classList.remove("hidden");
+    document.querySelector(".errormessages").classList.remove("hidden");
 
-    const $joinInterface = document.querySelector("#join-interface");
     // join button
-    $joinInterface.querySelector(".join-button").addEventListener("click", joinGame);
-
+    const $joinInterface = document.querySelector("#join-interface");
+    $joinInterface.querySelector(".join-button").addEventListener("click", checkExistingGames);
 }
 
 function renderCreate() {
-    document.querySelector("#start-interface").classList.add("hidden");
-    document.querySelector("#join-interface").classList.add("hidden");
-    document.querySelector("#create-interface").classList.remove("hidden");
-    // document.querySelector("#rules-interface").classList.remove("hidden");
+    document.querySelector(".errormessages p").innerHTML = "";
+    _$startInterface.classList.add("hidden");
+    _$joinInterface.classList.add("hidden");
+    _$createInterface.classList.remove("hidden");
+    // should this button be named create button? and have an ID instead of a class?
+    // kept it like this for css maybe??????? IDKKKKK
+    const $createInterface = document.querySelector("#create-interface");
+    $createInterface.querySelector(".join-button").addEventListener("click", checkInput);
 }
 
+function renderLobby(id, numberOfPlayers, playerNames) {
+    hideEverythingForLobby();
+    _$lobbyInterface.querySelector("span").innerText = id;
+    console.log(_$lobbyInterface);
+    const playersToJoin = numberOfPlayers - playerNames.length;
+    _$lobbyInterface.querySelector("p").innerText = "Waiting for " + playersToJoin + " more players to join.";
+    playerNames.forEach(player => {
+        const $templateClone = document.querySelector('template').content.firstElementChild.cloneNode(true);
+        $templateClone.querySelector('h3').innerText = player.name;
+        document.querySelector('#players').insertAdjacentHTML('beforeend', $templateClone.outerHTML);
+    });
+}
+// oke pls give me a better name xd
+function hideEverythingForLobby(){
+    _$joinInterface.classList.add("hidden");
+    _$createInterface.classList.add("hidden");
+    _$lobbyInterface.classList.remove("hidden");
 
-function renderIconPicker() {
-    document.querySelectorAll('img').forEach(item => {
+    _$createInterface.classList.add("hidden");
+    _$joinInterface.classList.add("hidden");
+    _$lobbyInterface.classList.remove("hidden");
+
+    _$lobbyInterface.querySelector("#players").innerText = "";
+}
+
+function renderIconPicker($clickedIcon) {
+    console.log($clickedIcon);
+    _$lobbyInterface.classList.add("hidden");
+    _$iconInterface.classList.remove("hidden");
+    _$iconInterface.querySelectorAll('img').forEach(item => {
         item.addEventListener('click', event => {
             const icon = event.target.id;
             // this is the currently selected icon.
+            _$lobbyInterface.classList.remove("hidden");
+            _$iconInterface.classList.add("hidden");
+            $clickedIcon.src = `assets/media/${icon}.png`;
             document.querySelector(`${previousScreen} button`).innerHTML = `<img src="assets/media/${icon}.png" alt="${icon}" id="${icon}">`;
         });
     });
 }
 
-
 function renderRules() {
-    console.log("Now in renderRules function");
+    _$startInterface.classList.add("hidden");
+    _$rulesInterface.classList.remove("hidden");
 }
 
 function backButton() {
-    document.querySelector("#create-interface").classList.add("hidden");
-    document.querySelector("#join-interface").classList.add("hidden");
-    document.querySelector("#start-interface").classList.remove("hidden");
+    _$createInterface.classList.add("hidden");
+    _$joinInterface.classList.add("hidden");
+    _$lobbyInterface.classList.add("hidden");
+    document.querySelector(".errormessages p").innerHTML = "";
+    _$startInterface.classList.remove("hidden");
 }
