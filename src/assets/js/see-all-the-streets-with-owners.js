a408cfd32e6152233cccda2911624140f0726c4a
Student <sibren.eeckhout@student.howest.be>
2022-03-30 14:10:03 +0200

    added a return button to go back to the index html file

--- src/assets/js/see-all-the-streets-with-owners.js
+++ src/assets/js/see-all-the-streets-with-owners.js
@@ -1,13 +1,13 @@
 "use strict";
-
+const playerstreetsdict = [];
 document.addEventListener('DOMContentLoaded',init);
 
 function init()
 {
-   let sortregular = "";
-   let streetnames = [];
-   fetchFromServer('/tiles','GET').then(tiles => runStreets(tiles, streetnames, sortregular));
+   const sortregular = "";
+   const streetnames = [];
    fetchFromServer('/games/dummy','GET').then(players => showPlayers(players.players));
+   fetchFromServer('/tiles','GET').then(tiles => runStreets(tiles, streetnames, sortregular));
    document.querySelector('form').addEventListener('input', searchstreet);
 }
 
@@ -18,8 +18,7 @@ function searchstreet(e)
     let sort = "";
     if (e.target.value)
     {
-        sort = e.target.value;
-        console.log(sort);
+        sort = e.target.value.toLowerCase();
         fetchFromServer('/tiles','GET').then(tiles => runStreets(tiles, streetnamessort, sort));
     }
     else
@@ -37,7 +36,7 @@ function runStreets(tiles, streetnames, sort)
     {}
     else if (street.name.includes("Chance"))
     {}
-    else if (street.name.includes(sort))
+    else if (street.name.toLowerCase().includes(sort))
     {
         streetnames.push(street);
     }
@@ -50,19 +49,34 @@ function runStreets(tiles, streetnames, sort)
 /* fill in the template with the api results*/
 function render_streets(street)
 {
+    let isbought = false;
     const $template = document.querySelector('template').content.firstElementChild.cloneNode(true);
     $template.querySelector('h2').innerText = street.name;
     $template.querySelector('li').innerText= "positie:  " + street.position;
     $template.querySelector('li+li').innerText= "cost:  " + street.cost;
     $template.querySelector('li+li+li').innerText= "mortage:  " + street.mortgage;
     $template.querySelector('li+li+li+li').innerText= "rent:  " + street.rent;
-    $template.querySelector(`div p`).innerText = "player:";
+    playerstreetsdict.forEach(player => {
+        let playername = player[0];
+        player.forEach(ownedstreet =>
+        {
+            if (ownedstreet === street.name)
+            {
+                isbought = true;
+                $template.querySelector(`div p`).innerText = "player: " + playername;
+            }
+        });
+    });
+    if (isbought === false)
+    {
+        $template.querySelector(`div p`).innerText = "player: not bought yet";
+    }
     document.querySelector('.templatediv').insertAdjacentHTML("beforeend", $template.outerHTML);
 }
 
+/* put the players with their owning streets in to a dictionary.*/
 function showPlayers(players)
 {
-    let playerstreetsdict = [];
     players.forEach(player =>
     {
         let playerstreets = [];
@@ -70,5 +84,6 @@ function showPlayers(players)
         playerstreets.push(player.properties.forEach(property => playerstreets.push(property.property)));
         playerstreetsdict.push(playerstreets);
     });
-    console.log(playerstreetsdict)
+    console.log(playerstreetsdict);
 }
+
