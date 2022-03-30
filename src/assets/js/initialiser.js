b0523d9e2b004d184ea75b0d25809301d5c923c2
xdNiBoR <51537026+xdNiBoR@users.noreply.github.com>
2022-03-30 13:10:14 +0200

    Fixed innit function with correct html page.

--- src/assets/js/initialiser.js
+++ src/assets/js/initialiser.js
@@ -4,12 +4,11 @@ let _gameID = null
 document.addEventListener('DOMContentLoaded',init);
 
 function init(){
-    initStartScreen();
-    // testConnection();
-    // renderMainPage();
+    if (document.querySelector("h1").id === "start-screen") {
+        initStartScreen();
+    }
+    else if (document.querySelector("h1").id === "main-screen"){
+        renderMainPage();
+    }
     // this is the innit for the main page
-}
-
-function testConnection(){
-    fetchFromServer('/tiles','GET').then(tiles => console.log(tiles)).catch(errorHandler);
 }
