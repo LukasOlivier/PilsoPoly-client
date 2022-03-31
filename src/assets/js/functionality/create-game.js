42717f7fd6828a32a61a8c97ac5091d9c7892547
xdNiBoR <51537026+xdNiBoR@users.noreply.github.com>
2022-03-31 23:04:12 +0200

    added check of names in create game

--- src/assets/js/functionality/create-game.js
+++ src/assets/js/functionality/create-game.js
@@ -5,22 +5,43 @@ function checkInput(){
     const name = {
         playerName: _$createInterface.querySelector(".name").value.toLowerCase()
     };
-    if (isNaN(numberOfPlayers)) {
-        errorHandler("Please provide a number between 2 and 8");
-        return;
-    } else if (numberOfPlayers > 8) {
-        errorHandler("There can only be 8 players");
-        return;
-    } else if (numberOfPlayers < 2) {
-        errorHandler("There need to be at least 2 players");
-        return;
-    } else if (name.playerName === "") {
-        errorHandler("Your name cant be empty");
-        return;
+    try {
+        const specialChar = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~1234567890é§]/;
+        if (isNaN(numberOfPlayers)) {
+            throw new Error("Please provide a number between 2 and 8");
+        } else if (numberOfPlayers > 8) {
+            throw new Error("There can only be 8 players");
+        } else if (numberOfPlayers < 2) {
+            throw new Error("There need to be at least 2 players");
+        } else if (name.playerName === "") {
+            throw new Error("Your name cant be empty");
+        } else if (specialChar.test(name.playerName) === true) {
+            throw new Error("Your name cant contain any special characters or numbers");
+        } else if (name.playerName.length > 10) {
+            throw new Error("Your name can only be 10 characters long");
+        }
+    }
+    catch (error) {
+        errorHandler(error);
     }
     createGame(numberOfPlayers, name);
 }
-
+// ask a teacher (vlummens :)) what s the best solution
+/*
+ if (isNaN(numberOfPlayers)) {
+            errorHandler("Please provide a number between 2 and 8");
+            return;
+        } else if (numberOfPlayers > 8) {
+            errorHandler("There can only be 8 players");
+            return;
+        } else if (numberOfPlayers < 2) {
+            errorHandler("There need to be at least 2 players");
+            return;
+        } else if (name.playerName === "") {
+            errorHandler("Your name cant be empty");
+            return;
+        }
+ */
 function createGame(numberOfPlayer, name){
     const body = {
         prefix: _config.prefix,
