3d613f06fa864cc260e44b5460cbaa2146aca984
Niels Soete <niels.soete@student.howest.be>
2022-04-02 22:37:54 +0200

    move the functions to move a player to a different file.

---
+++ src/assets/js/render/main-page/movePlayer.js
@@ -0,0 +1,59 @@
+"use strict";
+
+function move(value) {
+    const $button = document.querySelector("main button");
+    if ($button.classList.contains("hidden")) {
+        $button.classList.toggle("hidden");
+    }
+    _tempPlayerPositionID -= value;
+
+    if (_tempPlayerPositionID === 40) {
+        _tempPlayerPositionID = 0;
+    }
+
+    if (_tempPlayerPositionID === -1) {
+        _tempPlayerPositionID = 39;
+    }
+
+    removeCards();
+    getCardById(_tempPlayerPositionID);
+}
+
+function wheelEvent(e) {
+    if (e.deltaY < 0) {
+        moveRight();
+    } else {
+        moveLeft();
+    }
+}
+
+function keyPressEvent(e){
+    // a and q for move left for the fellow qwerty users..
+    if (e.key === 'ArrowRight' || e.key === 'd') {
+        moveRight();
+    } else if (e.key === 'ArrowLeft' || e.key === 'q' || e.key === 'a') {
+        moveLeft();
+    }
+}
+
+function moveLeft() {
+    move(1);
+}
+
+function moveRight() {
+    move(-1);
+}
+
+function backToCurrentPosition() {
+    document.querySelector("main button").classList.toggle("hidden");
+    _tempPlayerPositionID = 0;
+    removeCards();
+    getCardById(_playerPositionID);
+}
+
+function removeCards() {
+    const $articles = document.querySelectorAll("#cards-parent article");
+    $articles.forEach((article) => {
+        article.remove();
+    });
+}
--- src/assets/js/render/main-page/render-main-page.js
+++ src/assets/js/render/main-page/render-main-page.js
@@ -124,63 +124,8 @@ function checkKey(e) {
 
 
 // TODO: All move functions should be replaced to a different file..
-function move(value) {
-    const $button = document.querySelector("main button");
-    if ($button.classList.contains("hidden")) {
-        $button.classList.toggle("hidden");
-    }
-    _tempPlayerPositionID -= value;
 
-    if (_tempPlayerPositionID === 40) {
-        _tempPlayerPositionID = 0;
-    }
 
-    if (_tempPlayerPositionID === -1) {
-        _tempPlayerPositionID = 39;
-    }
-
-    removeCards();
-    getCardById(_tempPlayerPositionID);
-}
-
-function wheelEvent(e) {
-    if (e.deltaY < 0) {
-        moveRight();
-    } else {
-        moveLeft();
-    }
-}
-
-function keyPressEvent(e){
-    // a and q for move left for the fellow qwerty users..
-    if (e.key === 'ArrowRight' || e.key === 'd') {
-        moveRight();
-    } else if (e.key === 'ArrowLeft' || e.key === 'q' || e.key === 'a') {
-        moveLeft();
-    }
-}
-
-function moveLeft() {
-    move(1);
-}
-
-function moveRight() {
-    move(-1);
-}
-
-function backToCurrentPosition() {
-    document.querySelector("main button").classList.toggle("hidden");
-    _tempPlayerPositionID = 0;
-    removeCards();
-    getCardById(_playerPositionID);
-}
-
-function removeCards() {
-    const $articles = document.querySelectorAll("#cards-parent article");
-    $articles.forEach((article) => {
-        article.remove();
-    });
-}
 
 function renderPlayerProperties() {
     const playerProperties = loadFromStorage("playerProperties");
